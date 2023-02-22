1ad6b2ec602a6591de6aa92859d7bef60d4e9bab
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-02-22 17:33:55 +0100

    disable echoing of signals

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 static void	signalhandler(int sig);
 static void	print_lst(t_list *lst);
+static void	disable_echo(void);
 
 int	main(void)
 {
@@ -13,6 +14,7 @@ int	main(void)
 	lst = NULL;
 	while (1)
 	{
+		disable_echo();
 		input = readline("\033[0;31msigmashell \033[0;32m> \033[0;37m");
 		if (input == NULL)
 		{
@@ -25,6 +27,15 @@ int	main(void)
 		ft_lstclear(&lst, free);
 	}
 	return (EXIT_SUCCESS);
+}
+
+static void	disable_echo(void)
+{
+	struct termios	settings;
+
+	tcgetattr(STDIN_FILENO, &settings);
+	settings.c_lflag &= ~(IEXTEN | ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &settings);
 }
 
 static void	signalhandler(int sig)
