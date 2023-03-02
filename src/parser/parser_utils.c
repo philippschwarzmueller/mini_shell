e073ca95c38678569c30173ce31653d6444aa870
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-03-02 17:44:00 +0100

    add here_doc

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -46,3 +46,24 @@ void	free_cmd(void *pointer)
 	ft_free_stra(command->options);
 	free(command);
 }
+
+void	ft_here_doc(char *delimiter)
+{
+	char	*str;
+	int		src[2];
+
+	if (pipe(src))
+		ft_printf("here doc failed, errorcodes not yet existent\n");
+	str = get_next_line(0);
+	while (str && ft_strncmp(delimiter, str, ft_strlen(str) - 1))
+	{
+		ft_putstr_fd(str, 1);
+		free(str);
+		str = get_next_line(0);
+	}
+	if (str != NULL)
+		free(str);
+	dup2(src[0], 0);
+	close(src[0]);
+	close(src[1]);
+}
