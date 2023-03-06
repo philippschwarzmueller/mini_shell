3d0c0456914e5e0ff4a1e14cadfb7c9b4e44ec09
oph-design <ole.paul.heinzelmann@gmail.com>
2023-03-06 20:31:30 +0100

    implement first unset version

--- src/builtins/builtin_controller.c
+++ src/builtins/builtin_controller.c
@@ -32,7 +32,7 @@ int	builtin_controller_parent(t_command *cmd, char **env)
 	if (!ft_strncmp(str, "export", 7))
 		return (free(str), 1);
 	if (!ft_strncmp(str, "unset", 6))
-		return (free(str), 1);
+		return (free(str), ft_unset(&env, cmd->options), 1);
 	if (!ft_strncmp(str, "exit", 5))
 		return (free(str), 1);
 	return (free(str), 0);
