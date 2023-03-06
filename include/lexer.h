b96dddad5bf2167bef744b7971d818fb4d9f3cdc
oph-design <ole.paul.heinzelmann@gmail.com>
2023-03-06 19:11:55 +0100

    fix occasional segfaults

--- include/lexer.h
+++ include/lexer.h
@@ -28,8 +28,9 @@ typedef struct s_token
 }			t_token;
 
 void	del_token(void *content);
-void	remove_val(t_list **lst, void *content);
+//void	remove_val(t_list **lst, void *content);
 char	*ft_decrustify_str(char *str);
 void	set_type(t_token *new);
+void	del_first(t_list **lst);
 
 #endif
