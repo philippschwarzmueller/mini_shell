0d7434cf14eefa08c8e22ebb023044ea0fdda83d
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-03-06 09:51:06 +0100

    implement error handling for getcwd failure

--- src/buildins/pwd.c
+++ src/buildins/pwd.c
@@ -5,9 +5,11 @@ int	ft_pwd(char **options)
 	char	*cwd;
 
 	if (ft_stra_len(options) > 0)
-		return (ft_printf("pwd: too many arguments\n"), 1);
+		return (ft_printf("pwd: too many arguments\n"), EXIT_FAILURE);
 	cwd = NULL;
 	cwd = getcwd(cwd, 1);
+	if (cwd == NULL)
+		return (EXIT_FAILURE);
 	ft_printf("%s\n", cwd);
 	free(cwd);
 	return (EXIT_SUCCESS);
