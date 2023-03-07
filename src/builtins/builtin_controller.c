b354b9a97dd83c613640974f6f09b91d0476f97b
oph-design <ole.paul.heinzelmann@gmail.com>
2023-03-07 09:13:40 +0100

    move update_env to builtin_controller.c

--- src/builtins/builtin_controller.c
+++ src/builtins/builtin_controller.c
@@ -53,3 +53,43 @@ static char	*lower_str(char *str)
 	}
 	return (res);
 }
+
+int	update_env(char **env, char *varname, char *value)
+{
+	size_t	i;
+
+	i = 0;
+	while (env && env[i] && i < ft_stra_len(env))
+	{
+		if (ft_strncmp(varname, env[i], ft_strlen(varname)) == 0)
+		{
+			free(env[i]);
+			env[i] = ft_strjoin(varname, value);
+		}
+		i++;
+	}
+	if (env && env[i] && ft_strncmp(varname, env[i], ft_strlen(varname)))
+		env = add_to_environment(env, varname, value);
+	return (EXIT_SUCCESS);
+}
+
+char	**add_to_environment(char **env, char *varname, char *value)
+{
+	size_t		i;
+	char		**res;
+
+	i = 0;
+	if (env == NULL)
+		return (NULL);
+	res = malloc((ft_stra_len(env) + 2) * sizeof(char *));
+	while (env[i] != NULL)
+	{
+		res[i] = env[i];
+		i++;
+	}
+	res[i] = ft_strjoin(varname, value);
+	i++;
+	res[i] = NULL;
+	free(env);
+	return (res);
+}
