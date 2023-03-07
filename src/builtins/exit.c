d57ade2b63c94e7cd84550bfeaa3ded6719eac08
oph-design <ole.paul.heinzelmann@gmail.com>
2023-03-07 13:17:53 +0100

    add return from exit if commands are piped

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -2,7 +2,9 @@
 
 int	ft_exit(t_list *commands, char ***env)
 {
+	if (ft_lstsize(commands) > 1)
+		return (EXIT_SUCCESS);
 	ft_lstclear(&commands, free_cmd);
 	ft_free_stra(*env);
-	exit(0);
+	exit(EXIT_SUCCESS);
 }
