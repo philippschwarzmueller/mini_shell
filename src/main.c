434d6b2dfd4c2597ee0f7b5a93f333aacd249744
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-03-09 21:09:02 +0100

    prettify the ansi escape

--- src/main.c
+++ src/main.c
@@ -17,8 +17,8 @@ int	main(void)
 		if (input == NULL)
 		{
 			ft_putstr_fd("\x1b[1A", STDOUT_FILENO);
-			ft_putstr_fd("\033[12C", STDOUT_FILENO);
-			ft_putendl_fd(" exit", STDOUT_FILENO);
+			ft_putstr_fd("\033[13C", STDOUT_FILENO);
+			ft_putendl_fd("exit", STDOUT_FILENO);
 			clear_history();
 			ft_free_stra(env);
 			exit(EXIT_SUCCESS);
