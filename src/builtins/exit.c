26471a1228865495062b018d3b8e104327b84ca6
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-03-10 17:47:20 +0100

    handle errors with non numeric arg and too many args

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -2,6 +2,8 @@
 
 int	ft_exit(t_command *cmd, t_list *commands, char ***env)
 {
+	int	exit_code;
+
 	if (ft_lstsize(commands) > 1)
 		return (EXIT_SUCCESS);
 	if (cmd->options)
@@ -11,6 +13,14 @@ int	ft_exit(t_command *cmd, t_list *commands, char ***env)
 			ft_putendl_fd("exit: numeric argument required", STDERR_FILENO);
 			exit(255);
 		}
+		else if (ft_stra_len(cmd->options) > 1)
+		{
+			ft_putendl_fd("exit: too many arguments", STDERR_FILENO);
+			return (EXIT_FAILURE);
+		}
+		exit_code = ft_atoi(cmd->options[0]);
+		if (exit_code >= 0 && exit_code < 256)
+			exit(exit_code);
 	}
 	ft_lstclear(&commands, free_cmd);
 	ft_free_stra(*env);
