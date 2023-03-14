58dd2fcb2ea7d2c983121fc496d01b888ed1061f
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-03-14 11:07:58 +0100

    make decrustifystr non-static

--- include/expander.h
+++ include/expander.h
@@ -6,5 +6,6 @@ char	*trim_front(char *s1, const char *set);
 char	*trim_back(char *s1, const char *set);
 char	*remove_whitespaces(char *str);
 void	state_change(char c, t_state_lex *state);
+char	*ft_decrustify_str(char *str);
 
 #endif
