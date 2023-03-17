49b4be130edf4ec8fc382b85931458e1e3b8f2b5
oph-design <ole.paul.heinzelmann@gmail.com>
2023-03-17 08:56:49 +0100

    fix env variable in cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 
-int	ft_cd(char **options, char **env)
+int	ft_cd(char **options, char ***env)
 {
 	char	*old_pwd;
 	char	*new_pwd;
@@ -9,11 +9,11 @@ int	ft_cd(char **options, char **env)
 	if (!options || ft_strlen(options[0]) == 0)
 	{
 		new_pwd = ft_strdup("HOME");
-		new_pwd = get_env_value(new_pwd, env);
+		new_pwd = get_env_value(new_pwd, *env);
 		if (chdir(new_pwd) != 0)
 			return (free(old_pwd), EXIT_FAILURE);
-		update_env(&env, "OLDPWD=", old_pwd);
-		update_env(&env, "PWD=", new_pwd);
+		update_env(env, "OLDPWD=", old_pwd);
+		update_env(env, "PWD=", new_pwd);
 		return (free(old_pwd), EXIT_SUCCESS);
 	}
 	else if (chdir(options[0]) != 0)
@@ -23,7 +23,7 @@ int	ft_cd(char **options, char **env)
 		return (free(old_pwd), EXIT_FAILURE);
 	}
 	new_pwd = getcwd(NULL, 1);
-	update_env(&env, "OLDPWD=", old_pwd);
-	update_env(&env, "PWD=", new_pwd);
+	update_env(env, "OLDPWD=", old_pwd);
+	update_env(env, "PWD=", new_pwd);
 	return (free(old_pwd), free(new_pwd), EXIT_SUCCESS);
 }
