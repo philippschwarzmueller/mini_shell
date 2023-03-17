2a62329830b7a4037d2b4e9626fadd8cb877c853
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-03-17 17:22:09 +0100

    fix signal handling on childs

--- src/signal/signal_handling.c
+++ src/signal/signal_handling.c
@@ -23,7 +23,10 @@ static void	handle_sigint(int sig)
 {
 	if (sig == SIGINT)
 	{
-		write(STDERR_FILENO, "\n", 1);
+		write(STDIN_FILENO, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 		g_exit_code = 1;
 	}
 }
