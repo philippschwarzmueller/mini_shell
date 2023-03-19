8d122e847e3df2ff7233e12e01207168a8022054
oph-design <ole.paul.heinzelmann@gmail.com>
2023-03-19 16:54:24 +0100

    add print statements for exit in exit functionality

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,6 +1,8 @@
 #include "shell.h"
 
 static t_bool	is_num(char *s);
+static long		set_e_code(t_command *cmd);
+static int		checkoffset(const char *str);
 
 int	ft_exit(t_command *cmd, t_list *cmds, char ***env)
 {
@@ -9,39 +11,33 @@ int	ft_exit(t_command *cmd, t_list *cmds, char ***env)
 	if (ft_lstsize(cmds) > 1)
 		return (EXIT_SUCCESS);
 	if (cmd->options)
-	{
-		if ((cmd->options[0] && !is_num(cmd->options[0]))
-			|| !ft_strlen(cmd->options[0]))
-		{
-			ft_putendl_fd("exit: numeric argument required", STDERR_FILENO);
-			e_code = 255;
-		}
-		else if (ft_stra_len(cmd->options) > 1)
-		{
-			ft_putendl_fd("exit: too many arguments", STDERR_FILENO);
-			e_code = 1;
-		}
-		else
-			e_code = ft_strtol(cmd->options[0]);
-	}
+		e_code = set_e_code(cmd);
 	else if (ft_stra_len(cmd->options) == 0)
 		e_code = 0;
 	else
 		e_code = 1;
-	return (ft_lstclear(&cmds, free_cmd), ft_free_stra(*env), exit(e_code), 0);
+	ft_lstclear(&cmds, free_cmd);
+	ft_free_stra(*env);
+	if (isatty(STDIN_FILENO))
+		printf("exit\n");
+	exit(e_code);
+	return (0);
 }
 
-static int	checkoffset(const char *str)
+static long	set_e_code(t_command *cmd)
 {
-	unsigned int	i;
-
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\f'
-		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-	return (i);
+	if ((cmd->options[0] && !is_num(cmd->options[0]))
+		|| !ft_strlen(cmd->options[0]))
+	{
+		ft_putendl_fd("exit: numeric argument required", STDERR_FILENO);
+		return (255);
+	}
+	else if (ft_stra_len(cmd->options) > 1)
+	{
+		ft_putendl_fd("exit: too many arguments", STDERR_FILENO);
+		return (1);
+	}
+	return (ft_strtol(cmd->options[0]));
 }
 
 static t_bool	is_num(char *s)
@@ -58,4 +54,17 @@ static t_bool	is_num(char *s)
 		i++;
 	}
 	return (res);
+}
+
+static int	checkoffset(const char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\f'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	return (i);
 }
--- src/utils/signal_handling.c
+++ src/utils/signal_handling.c
@@ -36,3 +36,22 @@ void	init_child_sig_handler(void)
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 }
+
+void	control_exit(size_t prompt_size, char **env)
+{
+	char	*offset;
+	char	*exit_line;
+
+	clear_history();
+	ft_free_stra(env);
+	if (!isatty(STDIN_FILENO))
+		exit(g_exit_code);
+	offset = ft_strjoin_f(ft_itoa(prompt_size), "C");
+	exit_line = ft_strjoin("\033[", offset);
+	ft_putstr_fd("\x1b[1A", STDOUT_FILENO);
+	ft_putstr_fd(exit_line, STDOUT_FILENO);
+	ft_putendl_fd("exit", STDOUT_FILENO);
+	free(offset);
+	free(exit_line);
+	exit(g_exit_code);
+}
